8a375d45abdb763769d641692438fe476b6465bb
Minhajur rahman <minhajurrahman2716@gmail.com>
2026-02-08 15:40:49 +0600

    Add Responsive to Introduction section

--- src/app/Components/IntroductionSection.jsx
+++ src/app/Components/IntroductionSection.jsx
@@ -4,7 +4,7 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 
 const IntroductionSection = () => {
   return (
-    <div className=" flex flex-col-reverse md:flex-row items-center h-[130vh]   bg-secondary  py-[10vh] overflow-hidden ">
+    <div className=" flex flex-col-reverse md:flex-row items-center lg:h-[130vh]   bg-secondary  py-[10vh] overflow-hidden ">
       <div className="md:w-[55vw] lg:w-[55vw]   flex flex-col justify-between lg:mt-10">
         <div className="flex-1 flex flex-col justify-center items-center ">
           <div className="py-8   md:py-0 md:pb-0 w-[85%]  lg:h-[235.27px] md:px-10 mb-9">
@@ -30,7 +30,7 @@ const IntroductionSection = () => {
           height={771}
           alt="Girl holding flower image"
           sizes="(max-width: 768px) 250px, (max-width: 1024px) 350px, 513px"
-          className="rounded-t-[1000px]  w-[180px] md:h-[78vh]   lg:h-[90vh]  md:w-[300px] lg:w-[350px]"
+          className="rounded-t-[1000px]  w-[180px] md:h-[400px]   lg:h-[90vh]  md:w-[300px] lg:w-[350px]"
         ></Image>
         <div className=" w-[25vw] h-[25vw] sm:w-[20vw] sm:h-[20vw] md:w-[25vw] lg:w-[19vw] md:h-[25vw] lg:h-[19vw] rounded-full overflow-hidden absolute  bottom-0 md:-bottom-13 lg:-bottom-15  left-30 md:left-40 lg:left-55">
           <Image
